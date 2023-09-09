e62185dd64ce6074e6582a2c43b9db9dbb49a341
Natalia Strekalovskaia <nastrekalovskaya@edu.hse.ru>
2023-09-09 04:39:17 +0300

    Added README

--- hw_3_fix/chat_server.py
+++
@@ -1,9 +0,0 @@
-import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='session')
-
-channel.basic_publish(exchange='', routing_key='session', body='Hello World!')
-print('Sent Hello World!')
-connection.close()
